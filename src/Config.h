d8bd8aa1ec7fc18beecb36d15dc94c62e16abb64
Mohammad Durrani <mdurrani808@gmail.com>
2023-12-24 15:12:09 -0500

    removed bin and additional files

--- src/Config.h
+++
@@ -1,6 +0,0 @@
-namespace DriveConstants {
-	inline constexpr int kFrontLeftDriveMotorPort = 0;
-	inline constexpr int kRearLeftDriveMotorPort = 2;
-	inline constexpr int kFrontRightDriveMotorPort = 4;
-	inline constexpr int kRearRightDriveMotorPort = 6;
-}
